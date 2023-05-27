a952ca69642e69cf14c5569d4f738312b96bed6a
Auporna-Barua <aupornabarua@gmail.com>
2023-05-26 22:55:40 -0700

    privateroute add

---
+++ src/routes/PrivateRoutes.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+
+const PrivetRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <button className="btn btn-square loading bg-white border-none text-orange-400 font-bold"></button>
+      </div>
+    );
+  }
+
+  if (user) {
+    return children;
+  }
+  return <Navigate to={"/login"} state={{ from: location }} replace></Navigate>;
+};
+
+export default PrivetRoute;
