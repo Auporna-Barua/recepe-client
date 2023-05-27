8f6a291bcc502d98c75d888c8d1fa443a5da3acc
Auporna-Barua <aupornabarua@gmail.com>
2023-05-26 22:52:14 -0700

    authprovider add in our project auth system implement with help of firebase

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,76 @@
+import { createContext, useEffect, useState } from "react";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const google = new GoogleAuthProvider();
+  const gitHub = new GithubAuthProvider();
+
+  const createNewUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const logInUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const loginWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, google);
+  };
+
+  const loginWithGitHub = () => {
+    setLoading(true);
+    return signInWithPopup(auth, gitHub);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (loggedUser) => {
+      setUser(loggedUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const authData = {
+    user,
+    createNewUser,
+    logInUser,
+    loginWithGoogle,
+    loginWithGitHub,
+    logOut,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={authData}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
