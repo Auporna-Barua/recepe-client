f2046b8bb9ab26763becea8de26daddea885255e
Auporna-Barua <aupornabarua@gmail.com>
2023-05-26 22:41:30 -0700

    Error page or 404 page design

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,11 @@
+import img from "../../assets/confused-woman-have-error-message-computer_160308-4826.avif";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <img src={img} alt="" className="w-screen" />
+    </div>
+  );
+};
+
+export default ErrorPage;
